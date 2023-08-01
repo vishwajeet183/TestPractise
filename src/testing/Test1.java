dc451e981d03d7f6fb39b03b8f37125b7d6d8cd1
vishwajeet183 <vishu.gamepatil16@gmail.com>
2023-08-01 16:18:32 +0530

    initial commit

---
+++ src/testing/Test1.java
@@ -0,0 +1,18 @@
+package testing;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+				String name="Vishwajeet";
+				String password="Dnyankala@3";
+				System.out.println(name);
+			    System.out.println(password);
+			    System.out.println("Login Successfully completed");
+			    //java
+
+			
+
+	}
+
+}
